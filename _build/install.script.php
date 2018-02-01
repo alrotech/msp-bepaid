5000548d5572d7acd83dbf16b4a197e3fa02263c
Ivan Klimchuk <klimchuk@1pt.com>
2018-02-01 12:56:05 +0300

    Update package version

--- _build/install.script.php
+++ _build/install.script.php
@@ -33,7 +33,7 @@ $modx->setLogLevel(xPDO::LOG_LEVEL_ERROR);
 $modx->setLogTarget();
 
 $answer = $modx->runProcessor('workspace/packages/install',
-    ['signature' => 'mspbepaid-2.1.0-pl']
+    ['signature' => 'mspbepaid-2.3.0-pl']
 );
 
 $response = $answer->getResponse();
